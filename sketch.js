1106b1272e7ac504dfa5abdfe61e1ff53e248b18
Aayan1311 <87803250+Aayan1311@users.noreply.github.com>
2021-08-02 13:12:55 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,42 @@
+var box=createSprite=(200,200,20,20);
+
+
+
+function setup() {
+  createCanvas(400,400);
+}
+
+function draw(){
+  background(30);
+  if (keyIsDown(DOWN_ARROW))
+  {
+   background('red')
+ 
+ }
+ if (keyIsDown(UP_ARROW))
+  {
+   background('blue')
+ 
+ }
+ if (keyIsDown(RIGHT_ARROW))
+  {
+   background('yellow')
+ 
+ }
+
+ if (keyIsDown(LEFT_ARROW))
+  {
+   background('green')
+ 
+ }
+
+
+
+ drawSprites()
+
+}
+
+
+
+
+
